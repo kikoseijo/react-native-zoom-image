c06a4e2809bfc26aa5dbecbeb2cfd53b1e7600fa
Kiko Seijo <kiko@sunnyface.com>
2018-04-29 22:25:59 +0200

    Implementing Flow-

--- ZoomImage.js
+++ ZoomImage.js
@@ -1,8 +1,8 @@
 /**
  * Created by TinySymphony on 2017-03-23.
  */
-
-import React, { Component } from 'react';
+// @flow
+import * as React from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -24,18 +24,32 @@ const winRatio = winWidth / winHeight;
 
 const RCTUIManager = NativeModules.UIManager;
 
-class ZoomImage extends Component {
-  static propTypes = {
-    disabled: PropTypes.bool,
-    startCapture: PropTypes.bool,
-    moveCapture: PropTypes.bool,
-    responderNegotiate: PropTypes.func,
-    easingFunc: PropTypes.func,
-    rebounceDuration: PropTypes.number,
-    closeDuration: PropTypes.number,
-    showDuration: PropTypes.number,
-    enableScaling: PropTypes.bool
-  }
+type Props = {
+  source: {
+    uri: string
+  },
+  imgStyle: Object,
+  disabled: boolean,
+  startCapture: boolean,
+  moveCapture: boolean,
+  responderNegotiate: Function,
+  easingFunc: Function,
+  rebounceDuration: number,
+  closeDuration: number,
+  showDuration: number,
+  enableScaling: boolean,
+  resizeMode: any,
+  style: number,
+  minAlpha: any
+};
+type State = {
+  maxSize: {
+    width: number,
+    height: number
+  },
+  isModalVisible: boolean
+};
+class ZoomImage extends React.Component<Props, State> {
   static defaultProps = {
     disabled: false,
     startCapture: false,
@@ -45,8 +59,16 @@ class ZoomImage extends Component {
     showDuration: 100,
     easingFunc: Easing.ease,
     enableScaling: false
-  }
-  constructor(props) {
+  };
+  enableModal = false;
+  closeModal = this.closeModal.bind(this);
+  openModal = this.openModal.bind(this);
+  modalRefBind = this.modalRefBind.bind(this);
+  getMaxSizeByRatio = this.getMaxSizeByRatio.bind(this);
+  originPosition = {};
+  _modal = null;
+
+  constructor(props: Props) {
     super(props);
     this.state = {
       maxSize: {
@@ -55,61 +77,51 @@ class ZoomImage extends Component {
       },
       isModalVisible: false
     };
-    this.enableModal = false;
-    this.closeModal = this.closeModal.bind(this);
-    this.openModal = this.openModal.bind(this);
-    this.modalRefBind = this.modalRefBind.bind(this);
-    this.getMaxSizeByRatio = this.getMaxSizeByRatio.bind(this);
   }
-  getMaxSizeByRatio (ratio) {
+  getMaxSizeByRatio(ratio: number) {
     return {
       width: ratio >= winRatio ? winWidth : winWidth / ratio,
       height: ratio >= winRatio ? winWidth / ratio : winHeight
     };
   }
-  componentDidMount () {
+  componentDidMount() {
     if (this.props.source.uri) {
       Image.getSize(this.props.source.uri, (w, h) => {
-        this.setState((state) => {
+        this.setState(state => {
           state.maxSize = this.getMaxSizeByRatio(w / h);
           this.enableModal = true;
         });
       });
     } else {
-      this.setState((state) => {
+      this.setState(state => {
         state.maxSize = this.getMaxSizeByRatio(this.props.imgStyle.width / this.props.imgStyle.height);
         this.enableModal = true;
       });
     }
   }
-  openModal () {
+  openModal() {
     if (!this.refs.view || !this.enableModal || this.props.disabled) return;
     RCTUIManager.measure(findNodeHandle(this.refs.view), (x, y, w, h, px, py) => {
-      this.originPosition = {x, y, w, h, px, py};
+      this.originPosition = { x, y, w, h, px, py };
     });
     this.setState({
       isModalVisible: true
     });
   }
-  closeModal () {
+  closeModal() {
     if (this.props.disabled) return;
     this.setState({
       isModalVisible: false
     });
   }
-  modalRefBind (modal) {
+  modalRefBind(modal: any) {
     this._modal = modal;
   }
-  render () {
+  render() {
     return (
-      <TouchableWithoutFeedback style={this.props.imgStyle}
-        onPress={this.openModal}
-        ref="view">
+      <TouchableWithoutFeedback style={this.props.imgStyle} onPress={this.openModal} ref="view">
         <View style={this.props.style}>
-          <Image
-            source={this.props.source}
-            resizeMode={this.props.resizeMode}
-            style={this.props.imgStyle}/>
+          <Image source={this.props.source} resizeMode={this.props.resizeMode} style={this.props.imgStyle} />
           <ImageModal
             ref={this.modalRefBind}
             disabled={this.props.disabled}
@@ -173,29 +185,29 @@ class ImageModal extends Component {
       onShouldBlockNativeResponder: (evt, gestureState) => true
     });
   }
-  _onStartShouldSetPanResponder (evt, gestureState) {
+  _onStartShouldSetPanResponder(evt, gestureState) {
     // set responder for tapping when the drawer is open
     // TODO: tap close
     if (this._inAnimation || this.props.disabled) return;
     return false;
   }
-  _onMoveShouldSetPanResponder (evt, gestureState) {
+  _onMoveShouldSetPanResponder(evt, gestureState) {
     // custom pan responder condition function
     if (this._inAnimation || this.props.disabled) return;
-    if (this.props.responderNegotiate && this.props.responderNegotiate(evt, gestureState) === false) return false;
+    if (this.props.responderNegotiate && this.props.responderNegotiate(evt, gestureState) === false)
+      return false;
     if (this._touchPositionCheck(gestureState)) {
       return true;
     }
     return false;
   }
-  _handlePanResponderGrant(evt, gestureState) {
-  }
-  _handlePanResponderMove (evt, gestureState) {
-    const {dy} = gestureState;
+  _handlePanResponderGrant(evt, gestureState) {}
+  _handlePanResponderMove(evt, gestureState) {
+    const { dy } = gestureState;
     this._updateNativeStyles(dy);
   }
-  _handlePanResponderEnd (evt, gestureState) {
-    const {dy} = gestureState;
+  _handlePanResponderEnd(evt, gestureState) {
+    const { dy } = gestureState;
     if (dy > 0.4 * winHeight) {
       this._closeModal(true);
     } else if (-dy > 0.4 * winHeight) {
@@ -205,14 +217,14 @@ class ImageModal extends Component {
     }
   }
   _touchPositionCheck(gestureState) {
-    const {dx, dy} = gestureState;
+    const { dx, dy } = gestureState;
     if (Math.abs(dy) <= Math.abs(dx)) {
       return false;
     }
     return true;
   }
   _closeModal(isDown) {
-    const {easingFunc, onClose, closeDuration} = this.props;
+    const { easingFunc, onClose, closeDuration } = this.props;
     let current = this._contentStyle.style.top;
     this._inAnimation = true;
     new Animation({
@@ -220,7 +232,7 @@ class ImageModal extends Component {
       end: isDown ? winHeight : -winHeight,
       duration: closeDuration,
       easingFunc,
-      onAnimationFrame: (val) => {
+      onAnimationFrame: val => {
         this._updateNativeStyles(val);
       },
       onAnimationEnd: () => {
@@ -237,7 +249,7 @@ class ImageModal extends Component {
     this._closeModal(true);
   }
   _rebounce(isDown) {
-    const {rebounceDuration, easingFunc} = this.props;
+    const { rebounceDuration, easingFunc } = this.props;
     let current = this._contentStyle.style.top;
     this._inAnimation = true;
     new Animation({
@@ -245,7 +257,7 @@ class ImageModal extends Component {
       end: 0,
       duration: Math.abs(current / winHeight) * rebounceDuration,
       easingFunc,
-      onAnimationFrame: (val) => {
+      onAnimationFrame: val => {
         this._updateNativeStyles(val);
       },
       onAnimationEnd: () => {
@@ -254,10 +266,7 @@ class ImageModal extends Component {
     }).start();
   }
   _updateNativeStyles(dy) {
-    const {
-      width,
-      height
-    } = this.props.size;
+    const { width, height } = this.props.size;
     // this._contentStyle.style.left = dx;
     // this._contentStyle.style.right = -dx;
     this._contentStyle.style.top = dy;
@@ -282,44 +291,61 @@ class ImageModal extends Component {
     this.mask && this.mask.setNativeProps(this._modalStyle);
     this.img && this.img.setNativeProps(this._imgSize);
   }
-  componentDidUpdate () {
+  componentDidUpdate() {
     new Animation({
       start: 0,
       end: 1,
       duration: this.props.showDuration,
       easingFunc: Easing.ease,
-      onAnimationFrame: (val) => {
-        this.mask && this.mask.setNativeProps({style: {
-          opacity: val
-        }});
+      onAnimationFrame: val => {
+        this.mask &&
+          this.mask.setNativeProps({
+            style: {
+              opacity: val
+            }
+          });
       },
       onAnimationEnd: () => {
         this._inAnimation = false;
       }
     }).start();
   }
-  render () {
+  render() {
     const {
       visible,
       onClose,
       source,
-      size  // origin size of the image
+      size // origin size of the image
     } = this.props;
-    if (visible) { this._inAnimation = true; }
+    if (visible) {
+      this._inAnimation = true;
+    }
     this._initImgSize.style = size;
     return (
-      <Modal
-        visible={visible}
-        transparent={true}
-        onRequestClose={onClose}>
-        <View style={styles.mask} ref={mask => {this.mask = mask;}} {...this._pan.panHandlers}>
+      <Modal visible={visible} transparent={true} onRequestClose={onClose}>
+        <View
+          style={styles.mask}
+          ref={mask => {
+            this.mask = mask;
+          }}
+          {...this._pan.panHandlers}>
           <TouchableWithoutFeedback
-            ref={ref => {this.imgContainer = ref;}}
+            ref={ref => {
+              this.imgContainer = ref;
+            }}
             onPress={this._closeModalByTap}>
             <View
-              ref={ref => {this.content = ref;}}
+              ref={ref => {
+                this.content = ref;
+              }}
               style={styles.content}>
-              <Image ref={img => {this.img = img;}} source={source} style={[size, styles.img]}/>
+              <Image
+                ref={img => {
+                  this.img = img;
+                }}
+                source={source}
+                style={[size, styles.img]}
+              />
             </View>
           </TouchableWithoutFeedback>
         </View>
